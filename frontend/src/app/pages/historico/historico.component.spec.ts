78049bfb55236c93f1abafbb6e770bbd338be935
Rafael Lucio <rafaellucio.developer@gmail.com>
2025-10-25 21:23:01 -0300

    refactor: remove unused imports in component tests
    - Simplify test files for BeneficiosComponent, HistoricoComponent, and TransferenciasComponent by removing unused fakeAsync and flush imports.

--- frontend/src/app/pages/historico/historico.component.spec.ts
+++ frontend/src/app/pages/historico/historico.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { of, throwError } from 'rxjs';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
